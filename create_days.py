8200e3eb4fe770fccb4c76149eff16b876017d33
riggedCoinflip <tikalker@gmail.com>
2020-12-15 20:33:35 +0100

    wrote script to automatically create files from template and pull input from aoc website

--- create_days.py
+++ create_days.py
@@ -1,3 +1,35 @@
-for i in range(1,26):
-    with open(f'day{i}.py', mode='a') as a, open(f'day{i}_input.txt', mode='a') as b:
-        pass
+from pathlib import Path
+from datetime import date
+import aoc_helper
+
+def template() -> str:
+    with open('template.py') as template:
+        return template.read()
+
+def all_inputs():
+    for i in range(2015, 2021):
+        Path(f'{i}').mkdir(parents=True, exist_ok=True)
+        for j in range(1, 26):
+            with open(f'{i}/day{j}.py', mode='a+') as py:
+                if Path(f'{i}/day{j}.py').stat().st_size == 0:
+                    py.write(template().replace('{day}', str(j)))
+            with open(f'{i}/day{j}_input.txt', mode='a+') as txt:
+                if Path(f'{i}/day{j}_input.txt').stat().st_size == 0:
+                    txt.write(aoc_helper.fetch(j, i))
+
+
+def get_today():
+    # needs to be fixed for other timezones
+    today = date.today()
+    d, m, y = today.day, today.month, today.year
+    if m == 12:
+        with open(f'{y}/day{d}.py', mode='a+') as py:
+            if Path(f'{y}/day{d}.py').stat().st_size == 0:
+                py.write(template().replace('{day}', str(d)))
+        with open(f'{y}/day{d}_input.txt', mode='w') as txt:
+            txt.write(aoc_helper.fetch(d, y))
+        return
+    print('no AOC for today')
+
+if __name__ == '__main__':
+    get_today()
